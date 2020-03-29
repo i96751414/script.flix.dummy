146265d378f780ca5156d3520e8525f1b7bd3f8a
i96751414 <i96751414@gmail.com>
2020-03-29 21:32:37 +0100

    Add example of setResolvedUrl usage

--- lib/provider.py
+++ lib/provider.py
@@ -1,6 +1,8 @@
 from flix.kodi import ADDON_ICON
 from flix.provider import Provider, ProviderResult
 from xbmc import executebuiltin
+from xbmcgui import ListItem
+from xbmcplugin import setResolvedUrl
 
 
 # noinspection PyMethodMayBeStatic,PyUnusedLocal
@@ -22,10 +24,17 @@ class DummyProvider(Provider):
                 url="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
             ),
             ProviderResult(
-                label="Test fallback icon with provider_data",
+                label="Test fallback icon with provider_data (use setResolvedUrl)",
                 label2="Test fallback icon when no icon is provided",
                 provider_data={
-                    "url": "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.avi",
+                    "path": "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.avi",
+                }
+            ),
+            ProviderResult(
+                label="Test fallback icon with provider_data (return url)",
+                label2="This will then call resolve method (which will return the resolved url to Flix)",
+                provider_data={
+                    "url": "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.ogv",
                 }
             ),
         ]
@@ -40,11 +49,17 @@ class DummyProvider(Provider):
             ),
         ]
 
-    def resolve(self, provider_data):
+    def resolve(self, handle, item, provider_data):
         if isinstance(provider_data, dict):
             if "youtube" in provider_data:
                 executebuiltin("RunPlugin(plugin://plugin.video.youtube/play/?video_id={})".format(
                     provider_data["youtube"]))
+                return None
+            elif "path" in provider_data:
+                list_item = ListItem(item["title"], path=provider_data["path"])
+                list_item.setInfo("video", item["info"])
+                list_item.setArt(item["art"])
+                setResolvedUrl(handle, True, list_item)
                 return None
             elif "url" in provider_data:
                 return provider_data["url"]
