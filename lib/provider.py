86e397c9bf38b7e67fceac89fc5cdd89fef8277f
i96751414 <i96751414@gmail.com>
2020-04-04 16:01:28 +0100

    Remove handle from resolve method

--- lib/provider.py
+++ lib/provider.py
@@ -1,8 +1,5 @@
 from flix.kodi import ADDON_ICON
 from flix.provider import Provider, ProviderResult
-from xbmc import executebuiltin
-from xbmcgui import ListItem
-from xbmcplugin import setResolvedUrl
 
 
 # noinspection PyMethodMayBeStatic
@@ -31,17 +28,10 @@ class DummyProvider(Provider):
                 url="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
             ),
             ProviderResult(
-                label="Test fallback icon with provider_data (use setResolvedUrl)",
-                label2="Test fallback icon when no icon is provided",
-                provider_data={
-                    "path": "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.avi",
-                }
-            ),
-            ProviderResult(
                 label="Test fallback icon with provider_data (return url)",
                 label2="This will then call resolve method (which will return the resolved url to Flix)",
                 provider_data={
-                    "url": "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.ogv",
+                    "url": "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.avi",
                 }
             ),
             ProviderResult(
@@ -67,15 +57,10 @@ class DummyProvider(Provider):
             ),
         ]
 
-    def resolve(self, handle, provider_data):
+    def resolve(self, provider_data):
         if isinstance(provider_data, dict):
             if "youtube" in provider_data:
-                executebuiltin("RunPlugin(plugin://plugin.video.youtube/play/?video_id={})".format(
-                    provider_data["youtube"]))
-                return None
-            elif "path" in provider_data:
-                setResolvedUrl(handle, True, ListItem(path=provider_data["path"]))
-                return None
+                return "plugin://plugin.video.youtube/play/?video_id={})".format(provider_data["youtube"])
             elif "url" in provider_data:
                 return provider_data["url"]
 
